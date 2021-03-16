30909e58aeb9f5aff5037f830e5c975e7c4df912
MarufHimalay <m.u.maruf1999@gmail.com>
2021-03-16 22:14:27 +0600

    routing done

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>NOT FOUND!!!!</h2>
+            <h3>ERROR 404!</h3>
+        </div>
+    );
+};
+
+export default NotFound;
